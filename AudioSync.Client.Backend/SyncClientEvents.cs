158ff92d9b17a85870cab5b0b259ac4ff9e74f0c
yellowsink <yellowsink@protonmail.com>
2021-07-21 16:33:49 +0100

    oops

--- AudioSync.Client.Backend/SyncClientEvents.cs
+++ AudioSync.Client.Backend/SyncClientEvents.cs
@@ -8,8 +8,7 @@ namespace AudioSync.Client.Backend
 	{
 		private void SetupEvents()
 		{
-			// TODO: Uncomment these once we actually add handlers for them - else they crash
-			/*AddOneParamEvent("UpdateUser", UpdateUserEvent);
+			AddOneParamEvent("UpdateUser", UpdateUserEvent);
 			AddOneParamEvent("RemoveUser", RemoveUserEvent);
 			AddOneParamEvent("Play",       TransportPlayEvent);
 			AddOneParamEvent("Pause",      TransportPauseEvent);
@@ -18,7 +17,7 @@ namespace AudioSync.Client.Backend
 			AddOneParamEvent("Previous",   QueuePreviousEvent);
 			AddOneParamEvent("ClearQueue", QueueClearEvent);
 			AddTwoParamEvent("SetQueue", QueueSetEvent);
-			AddTwoParamEvent("Enqueue",  QueueAddEvent);*/
+			AddTwoParamEvent("Enqueue",  QueueAddEvent);
 		}
 
 		// Add an event with just a string, for the name of the user who performed the action
