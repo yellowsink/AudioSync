713e6dcde9a816ef155aa14e2fb56b2e1cfffa79
yellowsink <yellowsink@protonmail.com>
2021-07-21 20:36:59 +0000

    Bugfix

--- AudioSync.Client/Views/ToolDialog.axaml.cs
+++ AudioSync.Client/Views/ToolDialog.axaml.cs
@@ -31,7 +31,7 @@ namespace AudioSync.Client.Views
 		private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
 
 
-		private async void CheckForUpdates(object? sender = null, RoutedEventArgs routedEventArgs = null!)
+		private async void CheckForUpdates(object? sender = null, RoutedEventArgs e = null!)
 		{
 			((ToolDialogViewModel) DataContext!).Status        = "Checking for updates...";
 			((ToolDialogViewModel) DataContext!).EnableCheck   = false;
@@ -49,7 +49,7 @@ namespace AudioSync.Client.Views
 			((ToolDialogViewModel) DataContext!).EnableInstall = true;
 		}
 
-		private async void UpdateTools(object? sender = null, RoutedEventArgs routedEventArgs = null!)
+		private async void UpdateTools(object? sender = null, RoutedEventArgs e = null!)
 		{
 			((ToolDialogViewModel) DataContext!).Status        = "Installing YTDL...";
 			((ToolDialogViewModel) DataContext!).EnableCheck   = false;
@@ -61,6 +61,8 @@ namespace AudioSync.Client.Views
 			((ToolDialogViewModel) DataContext!).EnableCheck   = true;
 			((ToolDialogViewModel) DataContext!).EnableInstall = true;
 			((ToolDialogViewModel) DataContext!).EnableDone    = ToolManager.Versions.Ytdl != null;
+			
+			CheckForUpdates();
 		}
 
 		private void CloseButton(object? sender, RoutedEventArgs e) => Close();
